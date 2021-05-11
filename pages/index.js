fb4857302fda297edcace0d5e35b07c09a5a664e
Christopher Calvin <chrscalvn@gmail.com>
2021-05-11 21:42:40 +0700

    parsing data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+import Layout from '../component/layout/DefaultLayout'
 import Carousel from '../component/Carousel'
 import NewsBlock from '../component/NewsBlock'
 import BlockRenungan from '../component/BlockRenungan'
@@ -5,23 +7,25 @@ const axios = require('axios')
 import 'antd/dist/antd.css'
 
 export const getStaticProps = async () => {
-  const res = await axios.get('http://localhost:8000/post/1/latest')
-  const res2 = await axios.get('http://localhost:8000/post/2/latest')
+  const res = await axios.get('http://localhost:8000/posts/1/latest')
+  const res2 = await axios.get('http://localhost:8000/posts/2/latest')
   const posts = await res.data
   const renungan = await res2.data
   return {
-    props: {posts, renungan}
+    props: { posts, renungan }
   }
 }
 
-export default function Home({posts, renungan}) {
+export default function Home({ posts, renungan }) {
   return (
-    <div>
-      <Carousel />
+    <Layout pageTitle="Paroki Pringwulung">
       <div>
-        <NewsBlock posts={posts} />
-        <BlockRenungan renungan={renungan}/>
+        <Carousel />
+        <div>
+          <NewsBlock posts={posts} />
+          <BlockRenungan renungan={renungan} />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
